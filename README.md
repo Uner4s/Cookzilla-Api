206c8e9e9486473e810d11e6e4da63a11b1ca9c1
Uner4s <nicolas.p.poblete@gmail.com>
2017-06-27 15:03:19 -0400

    first commmit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Orionsoft GraphQL Server Boilerplate
+
+Installation
+
+```sh
+$ git clone https://github.com/orionsoft/server-boilerplate [name of your app]
+$ cd [name of your app]
+$ rm -rf .git
+$ yarn
+$ meteor npm install --save bcrypt
+$ meteor
+```
+
+
+> Project sponsored by [Orion Hosting](https://orion.hosting/?utm_source=github-server-boilerplate) - Hosting for Meteor
---
+++ server/imports/accounts/emailsConfig.js
@@ -0,0 +1,56 @@
+import {Accounts} from 'meteor/accounts-base'
+import getContent from 'api/emails/getContent'
+
+const isProduction = process.env.NODE_ENV === 'production'
+Accounts.emailTemplates.from = process.env.MAIL_FROM || 'test@orionsoft.io'
+Accounts.emailTemplates.siteName = 'Orionsoft'
+
+const getURL = function (path) {
+  return isProduction ? `http://admin.orionsoft.io${path}` : `http://localhost:3010${path}`
+}
+
+const getEmailTemplate = function (template, user, url) {
+  const data = {
+    user,
+    url,
+    homeUrl: getURL('/')
+  }
+
+  return getContent(template, data)
+}
+
+Accounts.emailTemplates.verifyEmail.subject = function (user) {
+  return 'Confirma tu email'
+}
+
+Accounts.emailTemplates.verifyEmail.html = function (user, url) {
+  return getEmailTemplate('auth_verify', user, url)
+}
+
+Accounts.urls.verifyEmail = function (token) {
+  return getURL('/verify-email/' + token)
+}
+
+Accounts.emailTemplates.resetPassword.subject = function (user) {
+  return 'Instrucciones para resetear la contraseña'
+}
+
+Accounts.emailTemplates.resetPassword.html = function (user, url) {
+  return getEmailTemplate('auth_reset', user, url)
+}
+
+Accounts.urls.resetPassword = function (token) {
+  return getURL('/reset/' + token)
+}
+
+Accounts.emailTemplates.enrollAccount.subject = function (user) {
+  return 'Se ha creado una cuenta para ti'
+}
+
+Accounts.emailTemplates.enrollAccount.html = function (user, url) {
+  return getEmailTemplate('auth_enroll', user, url)
+}
+
+Accounts.urls.enrollAccount = function (token) {
+  return getURL('/enroll/' + token)
+}
