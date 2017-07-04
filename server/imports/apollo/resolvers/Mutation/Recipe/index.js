f861dd41127b3f8359227562b4fc79b7e80392fb
Uner4s <nicolas.p.perez@gmail.com>
2017-07-04 00:21:15 -0400

    UpdateRecipe 1.1

--- server/imports/apollo/resolvers/Mutation/Recipe/index.js
+++ server/imports/apollo/resolvers/Mutation/Recipe/index.js
@@ -1,17 +1,12 @@
 import addRecipe from './addRecipe'
 import updateRecipe from './updateRecipe'
 import deleteRecipe from './deleteRecipe'
-<<<<<<< HEAD
 import updateRecipeImplement from './updateRecipeImplement'
 import updateRecipeIngredient from './updateRecipeIngredient'
 
 export default {
   updateRecipeIngredient,
   updateRecipeImplement,
-=======
-
-export default {
->>>>>>> 3d6c80c5903f7bd5df624ec54ccd51aacc7b0e35
   deleteRecipe,
   updateRecipe,
   addRecipe
