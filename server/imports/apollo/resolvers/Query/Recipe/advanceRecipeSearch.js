2ca391158c5b974b896154db9d15a2e1e4550ee8
Uner4s <nicolas.p.perez@gmail.com>
2017-07-18 01:03:50 -0400

    fixing problems

--- server/imports/apollo/resolvers/Query/Recipe/advanceRecipeSearch.js
+++ server/imports/apollo/resolvers/Query/Recipe/advanceRecipeSearch.js
@@ -7,11 +7,13 @@ export default function (root, {tools, ingredients}, {userId}) {
   // const variable = map(tools, tool => { return tool.name })
   // tool => {name: 'Olla'}
   // const toolsId = map(tools, tool => Tools.findOne(tool)._id)
+
   const toolsId = map(tools, tool => Tools.findOne(tool)._id)
   const ingredientsId = map(ingredients, ingredient => Ingredients.findOne(ingredient)._id)
-  console.log(toolsId, ingredientsId)
+  // console.log(toolsId, ingredientsId)
   // console.log(toolsid.map((tool) => toolsid.findOne({name: map(tools, tool => tool.name)})))
   // const ingredientsid = Ingredients.find({ingredientId: ingredient._id}).fetch()
+
   return Recipes.find({
     '$and': [
       {tool: {'$in': toolsId}},
