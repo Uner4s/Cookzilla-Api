621dd8648bb5f0758e875704e860371a5537ad94
Nicolás Fernando <nicolasfernando@orionsoft.io>
2017-10-14 01:52:56 -0300

    commit

--- server/imports/apollo/resolvers/Mutation/Ingredient/updateIngredient.js
+++ server/imports/apollo/resolvers/Mutation/Ingredient/updateIngredient.js
@@ -3,10 +3,7 @@ export default function (root, {_id, name}, {userId}) {
   Ingredients.update(_id, {$set: {
     name: name
   }})
-  // params.name
-  const ingredient = Ingredients.findOne(_id)
   // Const es una variable que no puede cambiar, no se puede sobre escribir.
   // FindOne es cuando tienes el id qiue quieres buscar.
-  // return Ingredients.findOne(ingredientId)
-  return ingredient
+  return Ingredients.findOne(_id)
 }
