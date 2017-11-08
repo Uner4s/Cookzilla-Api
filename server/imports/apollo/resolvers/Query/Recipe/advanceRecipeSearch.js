3bf109872b84a96c8e7d75e0eac67c08c4a2db35
Uner4s <nicolasfernando@orionsoft.io>
2017-11-08 00:35:57 -0300

    fixing

--- server/imports/apollo/resolvers/Query/Recipe/advanceRecipeSearch.js
+++ server/imports/apollo/resolvers/Query/Recipe/advanceRecipeSearch.js
@@ -2,18 +2,24 @@ import Recipes from 'server/imports/collections/Recipes'
 import Ingredients from 'server/imports/collections/Ingredients'
 import Tools from 'server/imports/collections/Tools'
 import map from 'lodash/map'
-export default function (root, {tools, ingredients}, {userId}) {
+export default function (root, { tools, ingredients }, { userId }) {
   // map(array, f(x))
   // const variable = map(tools, tool => { return tool.name })
   // tool => {name: 'Olla'}
   // const toolsId = map(tools, tool => Tools.findOne(tool)._id)
   const toolsAll = map(tools, tool => Tools.findOne(tool)) // Get the elements like an array, if dont exist, put undefined
-  const toolsOnly = toolsAll.filter(function (n) { return n !== undefined }) // Delete elements undefined in the array
+  const toolsOnly = toolsAll.filter(function (n) {
+    return n !== undefined
+  }) // Delete elements undefined in the array
   const toolBoolean = toolsOnly.filter(Boolean)
   const toolsId = map(toolBoolean, tool => tool._id) // Get only Id to do the search
 
-  const ingredientsAll = map(ingredients, ingredient => Ingredients.findOne(ingredient))  // Get the elements like an array, if dont exist, put undefined
-  const ingredientsOnly = ingredientsAll.filter(function (n) { return n !== undefined }) // Delete elements undefined in the array
+  const ingredientsAll = map(ingredients, ingredient =>
+    Ingredients.findOne(ingredient)
+  ) // Get the elements like an array, if dont exist, put undefined
+  const ingredientsOnly = ingredientsAll.filter(function (n) {
+    return n !== undefined
+  }) // Delete elements undefined in the array
   const ingredientBoolean = ingredientsOnly.filter(Boolean)
   const ingredientsId = map(ingredientBoolean, ingredient => ingredient._id) // Get only Id to do the search
 
@@ -24,10 +30,11 @@ export default function (root, {tools, ingredients}, {userId}) {
 
   if (toolsId !== '' && ingredientsId !== '') {
     return Recipes.find({
-      '$and': [
-        {tool: {'$in': toolsId}},
-        {ingredient: {'$in': ingredientsId}},
-        {state: { $exists: true }}
-      ]}).fetch()
+      $and: [
+        { tool: { $in: toolsId } },
+        { ingredient: { $in: ingredientsId } },
+        { state: { $exists: true } }
+      ]
+    }).fetch()
   }
 }
--- server/imports/apollo/resolvers/Types/Recipe/userLike.js
+++ server/imports/apollo/resolvers/Types/Recipe/userLike.js
@@ -1,7 +1,7 @@
-import {Meteor} from 'meteor/meteor'
-export default function (recipe, {id}, {userId}) {
+import { Meteor } from 'meteor/meteor'
+export default function (recipe, { id }, { userId }) {
   const userLike = recipe.like.map(function (variable) {
-    const user = Meteor.users.findOne({_id: variable})
+    const user = Meteor.users.findOne({ _id: variable })
     console.log(user.emails[0].address)
     return {
       email: user.emails[0].address
